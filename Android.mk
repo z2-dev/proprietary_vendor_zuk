0b506fe653313bd91e67728f4d32fea1ffe3ad6d
Álvaro Brey <alvaro.brv@gmail.com>
2017-05-19 14:10:12 +0200

    Remove Qualcomm time daemon, we are using sony timekeep now

--- Android.mk
+++ Android.mk
@@ -65,17 +65,6 @@ LOCAL_MODULE_SUFFIX := .apk
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := TimeService
-LOCAL_MODULE_OWNER := zuk
-LOCAL_SRC_FILES := proprietary/app/TimeService/TimeService.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := datastatusnotification
 LOCAL_MODULE_OWNER := zuk
 LOCAL_SRC_FILES := proprietary/app/datastatusnotification/datastatusnotification.apk
