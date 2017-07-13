c061328e32c685ff5baf09f64e9aafa7dc77b834
Cosme Domínguez Díaz <cosme.ddiaz@gmail.com>
2017-07-13 20:22:56 +0200

    video: Adreno blobs (LA.UM.5.5.r1-02500-8x96.0).

--- z2_row-vendor.mk
+++ z2_row-vendor.mk
@@ -25,5 +25,5 @@ PRODUCT_PACKAGES += \
     colorservice \
     qcrilhook
 
-$(call inherit-product, vendor/qcom/binaries/msm8996/graphics/graphics-vendor.mk)
+$(call inherit-product, vendor/zuk/z2_row/qcom/msm8996/graphics/graphics-vendor.mk)
 $(call inherit-product, vendor/zuk/z2_row/vendor/copyfiles.mk)
